a23bb3e74e30e6ca8b38b0cb68746169f85b0a01
Boris Vargas <borisvargaspaucara@gmail.com>
2026-04-17 17:10:33 -0400

    subiendo codigo

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,7 +1,89 @@
+import {
+    Heart,
+    ChevronLeft,
+    ChevronRight,
+    MoreHorizontal,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {CustomJumbotron} from "@/components/custom/CustomJumbotron.tsx";
+import {HeroStats} from "@/heroes/components/HeroStats.tsx";
+import {HeroGrid} from "@/heroes/components/HeroGrid.tsx";
+import {useState} from "react";
+
 export const HomePage = () => {
+
+    const {activeTab, setActiveTab} = useState<
+        'all' | 'favorites' | 'heroes' | 'villains'
+    >('all');
+
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <>
+                {/* Header */}
+                <CustomJumbotron
+                    title='Universo de Super Heroes'
+                    description='Descubre, explora y gestiona a tus superhéroes y villanos favoritos.'
+                />
+
+                {/* Stats Dashboard */}
+                <HeroStats />
+
+                {/* Tabs */}
+                <Tabs value={activeTab} className="mb-8">
+                    <TabsList className="grid w-full grid-cols-4">
+                        <TabsTrigger value="all">All Characters (16)</TabsTrigger>
+                        <TabsTrigger value="favorites" className="flex items-center gap-2">
+                            <Heart className="h-4 w-4" />
+                            Favorites (3)
+                        </TabsTrigger>
+                        <TabsTrigger value="heroes">Heroes (12)</TabsTrigger>
+                        <TabsTrigger value="villains">Villains (2)</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="all">
+                        <h1>Todos los personajes</h1>
+                    </TabsContent>
+                    <TabsContent value="favorites">
+                        <h1>Favoritos!!</h1>
+                    </TabsContent>
+                    <TabsContent value="heroes">
+                        <h1>Heroes</h1>
+                    </TabsContent>
+                    <TabsContent value="villains">
+                        <h1>Villanos</h1>
+                    </TabsContent>
+                    
+                </Tabs>
+
+                {/* Character Grid */}
+                <HeroGrid />
+
+                {/* Pagination */}
+                <div className="flex items-center justify-center space-x-2">
+                    <Button variant="outline" size="sm" disabled>
+                        <ChevronLeft className="h-4 w-4" />
+                        Previous
+                    </Button>
+
+                    <Button variant="default" size="sm">
+                        1
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        2
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        3
+                    </Button>
+                    <Button variant="ghost" size="sm" disabled>
+                        <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+
+                    <Button variant="outline" size="sm">
+                        Next
+                        <ChevronRight className="h-4 w-4" />
+                    </Button>
+                </div>
+            </>
+        </>
     )
 }
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,7 +1,21 @@
+import {CustomJumbotron} from "@/components/custom/CustomJumbotron.tsx";
+import {HeroStats} from "@/heroes/components/HeroStats.tsx";
+import {SearchControls} from "@/heroes/pages/search/ui/SearchControls.tsx";
+
 export const SearchPage = () => {
     return (
-        <div>
-            Search Page
-        </div>
+        <>
+          <CustomJumbotron
+              title='Busqueda de Super Heroes'
+              description='Descubre, explora y administra super heroes'
+          />
+
+          {/* Stats Dashboard */}
+          <HeroStats />
+
+          {/* ilter and Search */}
+          <SearchControls />
+
+        </>
     )
 }
